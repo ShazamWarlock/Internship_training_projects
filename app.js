215d10c082924d61bed5ad60e1c2654169cca9d5
Vrishir Iyer <74046329+ShazamWarlock@users.noreply.github.com>
2021-10-09 23:02:29 +0530

    get_elements fucntion

--- app.js
+++ app.js
@@ -1,41 +1,5 @@
-var arr = [1,2,3,4]
-console.log(arr)
-arr.push(5)
-console.log(arr)
-console.log(arr.length)
- var a=2;
- var b=10;
- console.log('a + b = ',a+b)
- console.log('a - b = ',a-b)
- console.log('a * b = ',a*b)
- console.log('a / b = ',a/b)
- console.log('a % b =',a%b)
-
- //assignment operators
- console.log('b ',b)
- b+=10;
- console.log('b+=10 ',b)
- b++
- console.log('b++ ',b)
- b-=1
- console.log('b++ ',b)
-
- var c=10
- console.warn(c++)
- console.log(c)
-
- //comparision operators
- var d=10
- var e=2
- var f='10'
- console.log(d>e)
- console.log(d<e)
- console.log(d>=e)
- console.log(d<=e)
- console.warn(d==f)
- console.warn(d===f) //selects type casting
- console.warn(d+f)
-
- //logical operators
- console.log('true&&true', true && false)
- console.log('true&&false', true && false)
+var heading = document.getElementById('heading')
+var para=document.getElementsByClassName('para')
+var tags=document.getElementsByTagName('a')
+var tags=document.querySelector('.para')
+console.log(tags)
