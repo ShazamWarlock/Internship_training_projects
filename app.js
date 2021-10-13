53a7d3e20fe7d219f7b85f5cf0d32c999f729090
Vrishir Iyer <74046329+ShazamWarlock@users.noreply.github.com>
2021-10-13 16:16:54 +0530

    Functions

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
-var heading = document.getElementById('heading')
-var para=document.getElementsByClassName('para')
-var tags=document.getElementsByTagName('a')
-var tags=document.querySelector('.para')
-console.log(tags)
+/*function greet(Name, age){
+    //console.log(Name, age)
+    console.log('Hello') 
+    console.log('Hey '+ Name + ". Your age is "+ age)
+}
+//greet('Vrishir',20)*/
+function add (a=0,b=0){
+    var sum=a+b
+    return sum
+}
+//result = add(10,20)
+//console.log(result)
+var add= (a=0,b=0)=>{
+    var sum=a+b
+    return sum
+}
+console.log(add(10,20))
