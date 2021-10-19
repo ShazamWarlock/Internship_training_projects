b83ecd6e1fc6a9db7133a2ba8db888e60e46775f
Vrishir Iyer <74046329+ShazamWarlock@users.noreply.github.com>
2021-10-19 16:35:47 +0530

    day-19

--- app.js
+++ app.js
@@ -9,7 +9,6 @@
     var sum=a+b
     return sum
 }
-
 console.log(add(10,20))
 //result = add(10,20)
 //console.log(result)
@@ -22,7 +21,6 @@ console.log(add(10,20))*/
 /*var add=(a=0,b=0)=>{
     return a+b
 }
-
 var add=(a=0,b=0)=>a+b
 console.log(add(10,20))*/
 
@@ -33,7 +31,6 @@ console.log(greet('Prabakaran'))*/
 
 //Call back FUNCTION
 /*var arr=['go to the GYM', 'Eat Food', 'Clean House']
-
 /*for(var i=0;i<arr.length;i++){
     console.log(arr[i])
 }*/
@@ -47,16 +44,26 @@ console.log(greet('Prabakaran'))*/
 })*/
 
 var button = document.querySelector('button')
-var body = document.querySelector('body')
+var input = document.querySelector('input')
+var list = document.querySelector('ul') 
+
+var chores = ['GYM']
+chores.includes('GYM')
 
 const callbackfunction = (event) =>{
-    //console.log(input.)
-    /*const inputValue = input.value
-    const element = document.createElement('li')
-    const textNode = document.createTextNode(inputValue)
-    element.appendChild(textNode)
-    list.appendChild(element)*/
-    body.classList.toggle('dark')
-}
+    //console.log(input.value)
+    const inputValue = input.value
+    if(chores.includes(input.value)){
+        console.log('already exists')
+    }
+    else{
+        chores.push(inputValue)
+        const element = document.createElement('li')
+        const textNode = document.createTextNode(inputValue)
+        element.appendChild(textNode)
+        list.appendChild(element)
+        element.addEventListener('click', (e) => { console.log(e.target.innerHTML) })
 
+    }
+}
 button.addEventListener('click', callbackfunction)
