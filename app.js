c031a04f10cfcfcc16593b5756695cc1b970b619
Vrishir Iyer <74046329+ShazamWarlock@users.noreply.github.com>
2021-10-22 01:50:11 +0530

    recipe

--- app.js
+++ app.js
@@ -121,3 +121,19 @@ fetchData()*/
 
 
 
+//challenge
+const button = document.querySelector('button')
+const input = document.querySelector('input')
+
+const getData = async () => {
+    console.log(item)
+    let url = `https://api.edamam.com/search?app_id=bd51454f&app_key=3b359328e30cad3141319969dfedaba9&q = ${item}`
+    console.log(url)
+    const res = await fetch(url)
+    const data = await res.json()
+    console.log(data)
+}
+//add event listener to the button
+button.addEventListener('click', () => {
+    console.log(input.value)
+})
