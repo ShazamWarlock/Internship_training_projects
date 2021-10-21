f8ff7e086c547f8dec073f9c890cb1d2b1286f57
Vrishir Iyer <74046329+ShazamWarlock@users.noreply.github.com>
2021-10-22 01:24:22 +0530

    first

--- app.js
+++ app.js
@@ -1,63 +1,123 @@
-/*console.log("one")
-
-setTimeout(function timeout(){
-    console.log("Timer 1");
-},2000)
-
-setTimeout(function timeout(){
-    console.log("Timer 2");
-},1000)
-
-console.log("two")
-console.log("two")
-console.log("two")
-console.log("two")
-console.log("two")
-console.log("two")
-console.log("two")*/
+//let response = fetch('https://jsonplaceholder.typicode.com/users')
+//.then(res => res.json())
 
 
-
-/*setTimeout(() => {console.log('Timer')}, 0)
-
-for(var i = 0; i<1000; i++){
-    console.log(i)
-}*/
-
-
-
-/*let value = new Promise((resolve, reject)=>{
-    setTimeout(() =>{
-        resolve('Hello')
-    }, 3000)
-})
-
-console.log(value)
-
-setTimeout(() => {
-    console.log(value)
-},4000)
-//console.log(value)*/
-
-
-
-/*let response = fetch('https://jsonplaceholder.typicode.com/users')
-
-console.log(response)
-
-setTimeout(() =>{
-    const data = response
-    console.log(response)
-    console.log(data,json())
-},2000)*/
-
-
-let response = fetch('https://jsonplaceholder.typicode.com/users')
-.then(res => res.json()).then(data => {
-    console.log(data)
-})
     
+/*const list = document.querySelector('ul')
+let arr = ['One', 'Two', 'Three', 'Four']
 
+console.log(list)
+arr.forEach(chore =>{
+    const element = document.createElement('li')
+    const textNode = document.createTextNode(chore)
+    element.appendChild(textNode)
+    element.addEventListener('click', (e) => {
+        console.log(e.target.innerHTML)
+        e.target.remove()
+    })
+    //console.log(element)
+    list.appendChild(element)
+})*/
+
+
+
+/*const list = document.querySelector('ul')
+let arr = ['One', 'Two', 'Three', 'Four']
+
+console.log(list)
+arr.forEach(chore =>{
+    const element = document.createElement('li')
+    const textNode = document.createTextNode(chore)
+    element.appendChild(textNode)
+    element.addEventListener('click', (e) => {
+            const useInput = window.prompt(`Are you sure u want to delete? ${chore}`)
+            if(useInput === 'YES' || useInput === 'yes')
+                e.target.remove()
+        })
+    //console.log(element)
+    list.appendChild(element)
+}) */
+
+
+
+
+/*let Name = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('Shazam')
+    },3000)
+})
+/*console.log(Name)
+setTimeout(() => {
+    console.log(Name)
+},4000)*/
+ 
+/*Name.then(name => {
+    console.log(name)
+})*/
+
+
+
+//MINI PROJECT
+/*ul = document.querySelector("ul");
+let response = fetch('https://jsonplaceholder.typicode.com/users')
+.then(resp => resp.json()).then(data => {
+    console.log(data);
+    append_data = (element) => {
+        lielement = document.createElement("li");
+        user_detail = element['name'] + element['username'] + element['email']
+        textnode = document.createTextNode(user_detail);
+        lielement.appendChild(textnode);
+        ul.appendChild(lielement)
+    }
+    data.forEach(append_data);
+})*/
+
+
+
+/*const table = document.querySelector('table')
+console.log(table)
+
+const createRow = (user)=>{
+    let newRow = document.createElement('tr')
+    let element = `
+            <td> ${user.id}</td>
+            <td> ${user.name}</td>
+            <td> ${user.email}</td>
+            <td> ${user.username}</td>`
+            newRow.innerHTML = element
+            return newRow
+}
+fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        data.forEach(user => {
+            table.appendChild(createRow(user))
+        })
+    })*/
+
+
+
+//problem with Promise
+/*fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        data.forEach(user => {
+            console.log(data)
+        })
+    }) */
+
+
+
+/*    const fetchData = async() => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users')
+    console.log(response)
+    const data = await response.json()
+    console.log(data)
+}
+
+fetchData()*/
 
 
 
