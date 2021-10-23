6a649091555b3671d62480b766f1dec4ff978407
Vrishir Iyer <74046329+ShazamWarlock@users.noreply.github.com>
2021-10-23 13:09:53 +0530

    first

--- app.js
+++ app.js
@@ -1,28 +1,59 @@
-//console.log(require('./index'))
-/*const stuff = require('./index')
+/*const button = document.querySelector('button')
+const input = document.querySelector('input')
+const div = document.querySelector('div')
 
-console.log(stuff)*/
+/*div.innerHTML = localStorage.getItem('InputValue')
 
-/*const { add, subtract, multiplication, division, message} = require('./index')
-
-add()
-subtract()
-multiplication()
-division()
-console.log(message)*/
+button.addEventListener('click', (req,res) => {
+    localStorage.setItem('InputValue',[1,2,3,4,5])
+    div.innerHTML = input.value
+})*/
 
 
-const obj1 = {
-    name: "McQueen"
-    email: "cars@cars.com"
-}
-//PUT
-const obj1 = {
-    name: "McQueen"
-    email: "cars@cars.com"
-}
-//PATCH
-const result = {
-    name: "McQueen"
-    email: "cars@cars.com"
-}
+/*const arr = [1,2,3,4,5]
+/*localStorage.setItem('val',arr)
+console.log(localStorage.getItem('val'))*/
+/*const val = JSON.stringify(arr)
+console.log(arr)
+console.log(val)*/
+/*localStorage.setItem('val', JSON.stringify(arr))
+console.log(JSON.parse(localStorage.getItem('val')))*/
+
+
+/*const path = require('path')
+console.log(path.resolve('./index.html'))
+console.log(path.relative('./index.js','./assets/css'))
+console.log(path.extname('./index.html'))
+console.log(path.dirname('./assets/css'))
+console.log(__dirname)
+console.log(path.join(__dirname,'app.js'))*/
+
+
+const fs = require('fs')
+/*fs.readFile('./test.txt', 'utf8', (data,error) => {
+    if(error)
+        console.log(error)
+    else
+        console.log(data)
+})
+
+console.log(fs.readFileSync('./test.txt', 'utf8'))
+
+console.log('I am a random log')*/
+
+
+
+
+/*fs.writeFile('./test.txt', 'New Stuff inside the file',
+(error) => {
+    if(error)
+        console.log(error)
+})*/
+
+
+fs.writeFile('./test.txt', `Append me ${fs.readFileSync('./test.txt','utf8')}`,
+(error) => {
+    if(error)
+        console.log(error)
+})
+
