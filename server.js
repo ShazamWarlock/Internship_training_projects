16194a284e8076dccecff6001e41a51b44b602b3
Vrishir Iyer <74046329+ShazamWarlock@users.noreply.github.com>
2021-10-28 00:56:27 +0530

    DAY-29

--- server.js
+++ server.js
@@ -1,73 +1,97 @@
 /*const express = require('express')
 const PORT = 3000
-
 const app = express()
 
-console.log(express)
+const Logger = (req,res,next) => {
+    console.log(req.method)
+    next()
+}
+
+app.use(express.json)     //automatically add middleware 
 
 app.get('/', (req,res) => {
-    res.send('GET')
+    res.send('Server Running')
 })
 
 app.post('/', (req,res) => {
-    res.send('POST')
+    console.log(req.body)
+    res.send('Server Running')
 })
 
-app.put('/', (req,res) => {
-    res.send('PUT')
-})
-
-app.patch('/', (req,res) => {
-    res.send('PATCH')
-})
-
-app.delete('/', (req,res) => {
-    res.send('DELETE')
-})
-
-/*app.get('/login', (req,res) => {
-    res.send('login form')
+app.listen(PORT, () => {
+    console.log(`Server running at port ${PORT}`)
 })*/
 
-/*app.listen(PORT, () => {
-    console.log(`Server running at port ${3000}`)
-})
-
-
-module.exports = () => {}*/
 
 
 
 /*const express = require('express')
 const PORT = 3000
-const app = express()*/
+const app = express()
 
-/*const verify = (req,res,next) => {
-    if(req.headers['user-agent'] === "Thunder Client (https://www.thunderclient.io)") next()
-    else res.send('BLOCKED')
-}*/
+app.use(express.json())     //automatically add middleware 
 
-/*const isAdmin = (req,res,next) => {
-    if(req.headers.admin === 'true') next()
-    else res.send("UNAUTHORISED")
+let products = [{name: 'iPhone 12 Case', price:'999'},{name: 'iPhone 13 Case', price:'1499'},{name: 'iPhone 13 Pro Case', price:'1999'}]
+
+//--------------------PUBLIC ROUTES--------------------
+//GET route
+//send products
+app.get('/products', (req,res) => {
+    res.json({products})
+})
+
+//--------------------PUBLIC ROUTES--------------------
+app.post('/products/add', (req,res) => {
+    console.log(req.body.name)
+    const newProduct = {
+        name: req.body.name,
+        price: req.body.price,
+    }
+    products.push(newProduct)
+    res.send('Product added')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server started at port ${PORT}`)
+})*/
+
+
+
+/*const express = require('express')
+const PORT = 3000
+const app = express()
+
+app.use(express.json())     //automatically add middleware 
+
+let products = [{name: 'iPhone 12 Case', price:'999'},{name: 'iPhone 13 Case', price:'1499'},{name: 'iPhone 13 Pro Case', price:'1999'}]
+
+//Middlewares
+const validator =(req, res) => {
+    const{name,price} = req.body
+    if(!name || !price) res.json({error: "Validation Failed"})
+    else next()
 }
 
-app.get('/public', (req,res) => {
-    console.log(req.headers)
-    res.send('This is a public route')
-    /*if(req.headers['user-agent'] === "Thunder Client (https://www.thunderclient.io") res.send('GET')
-    else res.send('BLOCKED')*/
-/*})
-
-app.get('/private', isAdmin, (req,res) => {
-    res.send('This is Admin route')
+//--------------------PUBLIC ROUTES--------------------
+//GET route
+//send products
+app.get('/products', (req,res) => {
+    res.json({products})
 })
 
-/*app.listen(PORT, () => {
-    console.log(`Server running at port ${3000}`)
+//--------------------PUBLIC ROUTES--------------------
+app.post('/products/add', validator, (req,res) => {
+    const{name,price} = req.body
+    products.push({
+    name,
+    price,
+    })
+    res.send({products})
 })
 
-module.exports = () => {}*/
+app.listen(PORT, () => {
+    console.log(`Server started at port ${PORT}`)
+})*/
 
 
 
@@ -75,15 +99,51 @@ const express = require('express')
 const PORT = 3000
 const app = express()
 
-app.use(express.json())
+app.use(express.json())     //automatically add middleware 
+const token = "TOP_SECRET"
+let products = [{name: 'iPhone 12 Case', price:'999'},{name: 'iPhone 13 Case', price:'1499'},{name: 'iPhone 13 Pro Case', price:'1999'}]
 
-app.post('/signup', (req,res) => {
-    console.log(req.body)
-    res.send('dadada')
+//Middlewares
+const validator =(req, res) => {
+    const{name,price} = req.body
+    if(!name || !price) res.json({error: "Validation Failed"})
+    else next()
+}
+
+const isAuthorised = (req,res,next) => {
+    if(req.headers.authorisation ===  token) next()
+    else res.json({ error: "UNAUTHORISED"})
+}
+
+//--------------------PUBLIC ROUTES--------------------
+//GET route
+//send products
+app.get('/products', (req,res) => {
+    res.json({products})
+})
+
+//--------------------PRIVATE ROUTES--------------------
+app.post('/products/add', isAuthorised, validator, (req,res) => {
+    const{name,price} = req.body
+    products.push({
+    name,
+    price,
+    })
+    res.send({products})
+})
+
+//-----------authentication
+app.post('/auth', (req,res) => {
+    const{email,password} = req.body
+    if(email === 'admin@gmail.com' && password === 'password'){
+        res.send({token})
+    }
+    else{
+        res.send({message: "UNAUTHORISED"})
+    }
+    res.send({products})
 })
 
 app.listen(PORT, () => {
-    console.log(`Server running at port ${PORT}`)
+    console.log(`Server started at port ${PORT}`)
 })
-
-module.exports = () => {}
