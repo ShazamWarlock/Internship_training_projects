2ac74f09923bbee084b64ed4f58bed43a4d05642
Vrishir Iyer <74046329+ShazamWarlock@users.noreply.github.com>
2021-11-05 00:21:56 +0530

    OCT17

---
+++ routes/categoryRoutes.js
@@ -0,0 +1,19 @@
+const express =  require('express')
+const database = require('../database/db')
+const router = express.Router()
+
+router.get('/all', (req,res) => {
+    try {
+        res.json({
+            categories: database.categories,
+            message: "Successfully fetched categories",
+            status: "SUCCESS"
+        })
+    } catch (error) {
+        res.json({
+            categories: [],
+            message: error.message,
+            status:"FAILED"
+        })
+    }
+})
--- server.js
+++ server.js
@@ -1,149 +1,32 @@
 /*const express = require('express')
-const PORT = 3000
 const app = express()
 
-const Logger = (req,res,next) => {
-    console.log(req.method)
-    next()
-}
+app.get('/products/cases')
+app.get('/products/protection')
+app.get('/products/bands')
 
-app.use(express.json)     //automatically add middleware 
-
-app.get('/', (req,res) => {
-    res.send('Server Running')
+app.get('/products/:categories', (req,res) => {
+    if(req.params.categories == 'band'){}
+    if(req.params.categories == 'case'){}
+    console.log(req.params)
+    res.send('PING')
 })
 
-app.post('/', (req,res) => {
-    console.log(req.body)
-    res.send('Server Running')
-})
 
-app.listen(PORT, () => {
-    console.log(`Server running at port ${PORT}`)
+app.listen(3001, () =>{
+    console.log("Listening at port 3001")
 })*/
-
-
 
 
 /*const express = require('express')
-const PORT = 3000
+const database = require('./database/db')
 const app = express()
+app.use(express.json())
 
-app.use(express.json())     //automatically add middleware 
+//database.products.push('iphone')
+//console.log(database)
 
-let products = [{name: 'iPhone 12 Case', price:'999'},{name: 'iPhone 13 Case', price:'1499'},{name: 'iPhone 13 Pro Case', price:'1999'}]
-
-//--------------------PUBLIC ROUTES--------------------
-//GET route
-//send products
-app.get('/products', (req,res) => {
-    res.json({products})
-})
-
-//--------------------PUBLIC ROUTES--------------------
-app.post('/products/add', (req,res) => {
-    console.log(req.body.name)
-    const newProduct = {
-        name: req.body.name,
-        price: req.body.price,
-    }
-    products.push(newProduct)
-    res.send('Product added')
-})
-
-app.listen(PORT, () => {
-    console.log(`Server started at port ${PORT}`)
+app.listen(3001, () =>{
+    console.log("Listening at port 3001")
 })*/
 
-
-
-/*const express = require('express')
-const PORT = 3000
-const app = express()
-
-app.use(express.json())     //automatically add middleware 
-
-let products = [{name: 'iPhone 12 Case', price:'999'},{name: 'iPhone 13 Case', price:'1499'},{name: 'iPhone 13 Pro Case', price:'1999'}]
-
-//Middlewares
-const validator =(req, res) => {
-    const{name,price} = req.body
-    if(!name || !price) res.json({error: "Validation Failed"})
-    else next()
-}
-
-//--------------------PUBLIC ROUTES--------------------
-//GET route
-//send products
-app.get('/products', (req,res) => {
-    res.json({products})
-})
-
-//--------------------PUBLIC ROUTES--------------------
-app.post('/products/add', validator, (req,res) => {
-    const{name,price} = req.body
-    products.push({
-    name,
-    price,
-    })
-    res.send({products})
-})
-
-app.listen(PORT, () => {
-    console.log(`Server started at port ${PORT}`)
-})*/
-
-
-
-const express = require('express')
-const PORT = 3000
-const app = express()
-
-app.use(express.json())     //automatically add middleware 
-const token = "TOP_SECRET"
-let products = [{name: 'iPhone 12 Case', price:'999'},{name: 'iPhone 13 Case', price:'1499'},{name: 'iPhone 13 Pro Case', price:'1999'}]
-
-//Middlewares
-const validator =(req, res) => {
-    const{name,price} = req.body
-    if(!name || !price) res.json({error: "Validation Failed"})
-    else next()
-}
-
-const isAuthorised = (req,res,next) => {
-    if(req.headers.authorisation ===  token) next()
-    else res.json({ error: "UNAUTHORISED"})
-}
-
-//--------------------PUBLIC ROUTES--------------------
-//GET route
-//send products
-app.get('/products', (req,res) => {
-    res.json({products})
-})
-
-//--------------------PRIVATE ROUTES--------------------
-app.post('/products/add', isAuthorised, validator, (req,res) => {
-    const{name,price} = req.body
-    products.push({
-    name,
-    price,
-    })
-    res.send({products})
-})
-
-//-----------authentication
-app.post('/auth', (req,res) => {
-    const{email,password} = req.body
-    if(email === 'admin@gmail.com' && password === 'password'){
-        res.send({token})
-    }
-    else{
-        res.send({message: "UNAUTHORISED"})
-    }
-    res.send({products})
-})
-
-app.listen(PORT, () => {
-    console.log(`Server started at port ${PORT}`)
-})
